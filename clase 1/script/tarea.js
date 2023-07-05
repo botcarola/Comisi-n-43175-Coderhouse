595652bac79baf48eb0ed44aec73695de7f008e1
botcarola <celerner4@gmail.com>
2023-07-04 22:03:45 -0300

    corrijo ejercicios

--- clase 1/script/tarea.js
+++ clase 1/script/tarea.js
@@ -5,8 +5,8 @@
 // 5) Con un cuadro de diálogo, pregúntele el nombre al usuario que ingresa a la web y guarde su valor en una variable.
 // 6) Visualizar en consola, junto al nombre del usuario, el siguiente mensaje: "Bienvenide a nuestra primera web -usuario-"
 // 7) Genere una nueva variable que almacene el número 5 y que se multiplique con el número impar.
-// 8) Visualice en consola cada uno de los nombres de las participantes del grupo.
-// 9) Reste 100 a la variable que multiplica por 5 al número primo.
+// 8) A través de un prompt, pregunte al usuario si es mayor de edad. Guarde este valor en una variable.
+// 9) Reste 100 a la variable que multiplica por 5 al número impar.
 // 10) Cotejar el tipo de dato del punto 3 y 5.
 
 // DESAFÍO PRIMER) hacer un programa que pida la edad del usuario y que devuelva en una alerta, el año en que nació
