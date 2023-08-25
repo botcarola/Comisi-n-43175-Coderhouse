ede690c29a3850b409a4cffa8270f837a5c3f41e
botcarola <celerner4@gmail.com>
2023-08-25 12:19:41 -0300

    tarea asincronismo

---
+++ tarea 25/8 fetch/index.js
@@ -0,0 +1,30 @@
+// COMO SUGERENCIA PRINCIPAL PARA LA REALIZACIÓN DE ESTAS ACTIVIDADES, PROPONGO QUE LEAN LA DOCUMENTACIÓN DE LA API DE RICK AND MORTY: https://rickandmortyapi.com/
+// Todo lo planteado en estas actividades ya ha sido visto en clases, puedo ayudarse con el código del repo. 
+
+// **Actividad 1: Mostrar Tarjetas de Personajes**
+// 1. Crea una página HTML con las estructuras necesarias para mostrar las tarjetas de personajes.
+// 2. Utiliza CSS para dar estilo a las tarjetas y hacerlas visualmente atractivas.
+// 3. Usa JavaScript para hacer una solicitud a la API de personajes: `https://rickandmortyapi.com/api/character`.
+// 4. Procesa los datos obtenidos y genera tarjetas individuales para cada personaje que muestren su nombre, estado, especie, tipo, género, imagen y ubicación.
+
+// **Actividad 2: Implementar Formulario de Búsqueda**
+// 1. Agrega un formulario a la página con un campo de entrada (input) para que los usuarios puedan buscar personajes por nombre.
+// 2. Cuando se envíe el formulario, captura el valor del campo de entrada.
+// 3. Utiliza JavaScript para realizar una solicitud a la API de personajes con el nombre proporcionado en la URL: `https://rickandmortyapi.com/api/character/?name=${ACÁ VA EL VALOR DEL INPUT}`.
+// 4. Imprime los datos obtenidos desde la solicitud en la consola.
+
+// **Actividad 3: Actualizar el DOM con Resultados de Búsqueda**
+// 1. Crea un área en la página donde se mostrarán las tarjetas de personajes buscados.
+// 2. Una vez que se obtengan los resultados de la búsqueda desde la API, procesa los datos y genera nuevas tarjetas de personajes, similar a la actividad 1.
+// 3. Utiliza JavaScript para actualizar el DOM con las nuevas tarjetas generadas en respuesta a la búsqueda.
+
+// **Actividad 4: Mejorando la Interfaz de Usuario**
+// 1. Implementa una función para que, al realizar una búsqueda, se eliminen las tarjetas anteriores y solo se muestren las tarjetas correspondientes a la búsqueda actual sobre el mismo contenedor de las tarjetas anteriores (sobreescribir el contenido del contenedor).
+// 2. Agrega animaciones o transiciones CSS para hacer que la actualización de tarjetas sea visualmente agradable. OPCIONAL
+// 3. Considera implementar paginación si la API proporciona múltiples páginas de resultados. LO VEMOS EN EL AFTER
+
+// **Actividad 5: Opcional - Detalles del Personaje**
+// 1. Agrega un evento a las tarjetas de personajes para que, al hacer clic en una tarjeta, se muestren más detalles del personaje. OPCIONAL
+// 2. Utiliza JavaScript para hacer una solicitud a la API de detalles del personaje usando la URL proporcionada en los datos del personaje.
+// 3. Muestra los detalles adicionales (como episodios en los que aparece el personaje) en una sección adicional en la tarjeta.
+
